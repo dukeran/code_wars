5415bf21c9fdb026cb681d8a7ca79cbbc9031b36
Kerrie Durham <languistics@gmail.com>
2015-12-19 20:03:42 -0500

    improved README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # code_wars
+The files necessary to start my blog on coding! 
+Life is not a spectator sport!
+end and commit! streak time 
--- class Do_turn.rb
+++ class Do_turn.rb
@@ -1,3 +1,16 @@
+
+Instructions:
+Terminal game turn function
+
+You are creating a text-based terminal version of your favorite board game. 
+In the board game, each turn has six steps that must happen in this order: 
+roll the dice, move, combat, get coins, buy more health, and print status.
+
+You are using a library that already has the functions below. Create a function 
+named doTurn/do_turn that calls the functions in the proper order as described 
+in the paragraph above.
+
+WOULD NOT WORK :
 class Do_turn
  def initialize(player)
     @player = player
@@ -13,3 +26,19 @@ class Do_turn
    end
 end 
 end 
+
+Tried wayyyy to hard without fully reading the instructions. 
+Correct answer: 
+def do_turn
+    roll_dice
+    move
+    combat
+    get_coins
+    buy_health
+    print_status
+end
+
+MoeSadoon's method: 
+def do_turn
+  return roll_dice, move, combat, get_coins, buy_health, print_status 
+end
